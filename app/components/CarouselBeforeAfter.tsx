04fd68b54c377062e22218050107e600ed96e970
GaspardPcht <gaspardpauchet@gmail.com>
2024-11-23 10:49:50 +0100

    Ajout de la directive 'use client' dans les composants CarouselBeforeAfter et ContactForm pour activer le rendu côté client.

--- app/components/CarouselBeforeAfter.tsx
+++ app/components/CarouselBeforeAfter.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { Dispatch, SetStateAction, useState } from 'react';
 import { motion } from 'framer-motion';
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
